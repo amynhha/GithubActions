d0bf6d44a1ea7fb7d9c49f0cc4bd172e66812c07
amynhha <142844316+amynhha@users.noreply.github.com>
2025-03-27 18:35:41 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,55 +1,137 @@
-using System;
-using NUnit.Framework;
+namespace GithubActionsLab;
 
-namespace GithubActionsLab
+[TestClass]
+public class Addition
 {
-    [TestFixture]
-    public class Math
-    {
-        [Test]
-        public void Add_Valid_Nguyen()
-        {
-            Assert.AreEqual(3, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("5", "7"));
-        }
+	[TestMethod]
+	public void Add_Valid_Nguyen()
+	{
+		Assert.AreEqual(3, Program.Add("1", "2"));
+		Assert.AreEqual(5, Program.Add("3", "2"));
+		Assert.AreEqual(12, Program.Add("5", "7"));
+	}
 
-        [Test]
-        public void Add_Invalid_Nguyen()
-        {
-            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
-        }
+	[TestMethod]
+	public void Add_Invalid_Nguyen()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
+	}
 
-        [Test]
-        public void Add_Null_Nguyen()
-        {
-            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
-        }
+	[TestMethod]
+	public void Add_Null_Nguyen()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+	}
+}
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Nguyen()
+	{
+		Assert.AreEqual(-1, Program.Subtract("1", "2"));
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(-2, Program.Subtract("5", "7"));
+	}
 
-        // Implement 3 tests per operation, following a similar pattern as above
-        [Test]
-        public void Subtract_Valid_Nguyen() {
-            Assert.AreEqual(2, Program.Subtract("5", "3"));
-            Assert.AreEqual(0, Program.Subtract("3", "3"));
-            Assert.AreEqual(-4, Program.Subtract("1", "5"));
-        }
+	[TestMethod]
+	public void Subtract_Invalid_Nguyen()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
 
-        [Test]
-        public void Multiply_Valid_Nguyen() {
-            Assert.AreEqual(6, Program.Multiply("2", "3"));
-            Assert.AreEqual(0, Program.Multiply("5", "0"));
-            Assert.AreEqual(-8, Program.Multiply("-2", "4"));
-        }
+	[TestMethod]
+	public void Subtract_Null_Nguyen()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Multiply_Valid_Nguyen()
+	{
+		Assert.AreEqual(2, Program.Multiply("1", "2"));
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
+		Assert.AreEqual(35, Program.Multiply("5", "7"));
+	}
 
-        [Test]
-        public void Power_Valid_Nguyen() {
-            Assert.AreEqual(8, Program.Power("2", "3"));
-            Assert.AreEqual(1, Program.Power("5", "0"));
-            Assert.AreEqual(0.25, Program.Power("2", "-2"));
-        }
-    }
+	[TestMethod]
+	public void Multiply_Invalid_Nguyen()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Multiply_Null_Nguyen()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid_Nguyen()
+	{
+		Assert.AreEqual(0.5, Program.Divide("1", "2"));
+		Assert.AreEqual(1.5, Program.Divide("3", "2"));
+		Assert.AreEqual(0.7142857142857143, Program.Divide("5", "7"), 0.0000001); // Added tolerance for floating point comparison
+	}
+
+	[TestMethod]
+	public void Divide_Invalid_Nguyen()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Divide_Null_Nguyen()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
+[TestClass]
+public class Power
+{
+	[TestMethod]
+	public void Power_Valid_Nguyen()
+	{
+		Assert.AreEqual(1, Program.Power("1", "0"));
+		Assert.AreEqual(4, Program.Power("2", "2"));
+		Assert.AreEqual(128, Program.Power("2", "7"));
+	}
+
+	[TestMethod]
+	public void Power_Invalid_Nguyen()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+	}
+
+	[TestMethod]
+	public void Power_Null_Nguyen()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+	}
 }
